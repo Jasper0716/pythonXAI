5237bcdd4ecca74ca8ed608edfa08ba021b2be37
jasper0716 <kaifan@gmail.com>
2025-08-04 17:02:09 +0800

    新增 main.py 和 CLASS1-1.PY，包含基本 Python 語法範例

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+print(hello world)
+print("Hello, World!")
+print("This is a Python script.")
 
-print("hello")
+
+
+print("Let's write some code.")
+
+print("This is a simple example of Python code.")
+print("Goodbye, World!")
+print("End of the script.")
+
+print("This is the end of the main.py file.")
+print("Thank you for running this script!")
+print("Have a great day!")
+print("This is a continuation of the script.")
+print("We are adding more functionality.")
+      
