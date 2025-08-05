ca348b16531a1679c46d23118058807516aa1a2c
jasper0716 <kaifan@gmail.com>
2025-08-05 13:55:30 +0800

    重構 main.py，移除多餘的 print 語句，並新增標題顯示功能；新增 CLASS1-2.py，包含多種 Streamlit 顯示範例

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-print(hello world)
-print("Hello, World!")
-print("This is a Python script.")
+import streamlit as st
 
-
-
-print("Let's write some code.")
-
-print("This is a simple example of Python code.")
-print("Goodbye, World!")
-print("End of the script.")
-
-print("This is the end of the main.py file.")
-print("Thank you for running this script!")
-print("Have a great day!")
-print("This is a continuation of the script.")
-print("We are adding more functionality.")
-      
+st.title("discord jasper123")
